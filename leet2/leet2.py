3853029bdd9063460558743d2acc06c0a5a092de
vaibhav7766 <vaibhavsharmav777@gmail.com>
2024-07-10 22:03:23 +0530

    modified:   leet2/leet2.py

--- leet2/leet2.py
+++ leet2/leet2.py
@@ -1,30 +1,32 @@
-#https://leetcode.com/problems/add-two-numbers/submissions/
+# https://leetcode.com/problems/add-two-numbers/
 
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-        
-class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        s1 = ""
-        temp = l1
-        while temp:
-            s1 += str(temp.val)
-            temp = temp.next
-        s2 = ""
-        temp = l2
-        while temp:
-            s2 += str(temp.val)
-            temp = temp.next
-        num1 = int(s1[::-1])
-        num2 = int(s2[::-1])
-        result = num1 + num2
-        result_str = str(result)[::-1]
-        dummy = ListNode()
-        current = dummy
-        for digit in result_str:
-            current.next = ListNode(int(digit))
-            current = current.next
 
-        return dummy.next
+class Solution:
+    def print_ll(self, ll: ListNode) -> list:
+        s = ""
+        temp = ll
+        while temp:
+            s += str(temp.val)
+            temp = temp.next
+        return s
+
+    def str_to_ll(self, s: str) -> ListNode:
+        if not s:
+            return None
+        ll = ListNode(s[0])
+        temp = ll
+        for i in range(1, len(s)):
+            temp.next = ListNode(s[i])
+            temp = temp.next
+        return ll
+
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        s1 = int(self.print_ll(l1)[::-1])
+        s2 = int(self.print_ll(l2)[::-1])
+        ans = str(s1 + s2)[::-1]
+
+        return self.str_to_ll(ans)
